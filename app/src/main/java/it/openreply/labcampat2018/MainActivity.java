2e2c452aecc3ba538d86cbbf94c8d0c2a93389f1
Giovanni Di Gialluca <gdigialluca@MacBook-Pro-di-Giovanni-2.local>
2018-04-17 23:25:09 +0200

    Led integration

--- app/src/main/java/it/openreply/labcampat2018/MainActivity.java
+++ app/src/main/java/it/openreply/labcampat2018/MainActivity.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.PeripheralManager;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -29,6 +31,9 @@ import java.util.List;
  */
 public class MainActivity extends Activity {
 
+    private Gpio ledGpio;
+    private static String LED_GPIO = "BCM26";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,5 +42,48 @@ public class MainActivity extends Activity {
         PeripheralManager manager = PeripheralManager.getInstance();
         List<String> gpios = manager.getGpioList();
         Log.d("LAB", gpios.toString());
+
+        try {
+            ledGpio = manager.openGpio(LED_GPIO);
+            ledGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+            //Start blinking for test
+            blinkLED();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+
+    /**
+     * blink the led 5 times
+     */
+    private void blinkLED() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    try {
+                        if (ledGpio != null)
+                            ledGpio.setValue(i % 2 == 0);
+                        Thread.sleep(500);
+                    } catch (Exception e) { /* uh! uh!*/ }
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        //Close Led GPIO
+        if (ledGpio != null) {
+            try {
+                ledGpio.close();
+                ledGpio = null;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onDestroy();
+    }
+
 }
